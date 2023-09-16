a7ec320397df67776855a6db3b84fab9d597ef96
Herryawan Wiguna <wigunaherryawan@gmail.com>
2023-09-16 21:52:56 +0700

    Banner can tap to promo and fix news layout

--- lib/features/promo/data/models/banner.dart
+++ lib/features/promo/data/models/banner.dart
@@ -2,10 +2,12 @@ import '../../domain/entities/banner.dart';
 
 class BannerModel extends BannerEntity {
   const BannerModel({
+    String? id,
     String? title,
     String? bannerImage,
     String? startDate,
   }) : super(
+          id: id,
           title: title,
           bannerImage: bannerImage,
           startDate: startDate,
@@ -13,6 +15,7 @@ class BannerModel extends BannerEntity {
 
   factory BannerModel.fromJson(Map<String, dynamic> data) {
     return BannerModel(
+      id: data['id'],
       title: data['title'],
       bannerImage: data['banner_image'],
       startDate: data['start_date'],
